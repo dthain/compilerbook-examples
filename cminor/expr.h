231b6a6b86aab6406dacdf9ada0a2aa40882d4cb
Douglas Thain <dthain@nd.edu>
2017-10-25 09:18:57 -0400

    expr_create_char_literal should take a char.

--- cminor/expr.h
+++ cminor/expr.h
@@ -29,7 +29,7 @@ struct expr * expr_create( expr_t kind, struct expr *left, struct expr *right );
 struct expr * expr_create_name( const char *n );
 struct expr * expr_create_boolean_literal( int c );
 struct expr * expr_create_integer_literal( int c );
-struct expr * expr_create_character_literal( int c );
+struct expr * expr_create_character_literal( char c );
 struct expr * expr_create_string_literal( const char *str );
 
 void expr_print( struct expr *e );
